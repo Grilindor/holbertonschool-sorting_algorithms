56baad8ce4da3151a6704a84c4926b90f8432b35
Jlaborne <laborne.j@hotmail.fr>
2024-04-10 13:25:13 +0000

    Add sort.h header file (prototypes and structure) + Task 0 0. Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+/**
+ * swap_ints - Swap two elements of an array.
+ * @a: The first element to swap.
+ * @b: The second element to swap.
+ */
+void swap_elements(int *a, int *b)
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ * @array: pointer to an array of integer
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		print_array(array, size);
+
+		if (array[i] > array[i + 1])
+			swap_elements (array[i], array[i + 1]);
+	}
+}
