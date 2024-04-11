a905ba73176ff187a2648531813729509eef8658
Grilindor <marcor91@hotmail.fr>
2024-04-11 15:22:18 +0000

    task1 change for to a while, and correcting betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,41 +1,22 @@
 #include "sort.h"
 
 /**
-* dlistint_len - a function that returns the number
-* of elements in a linked list
-*@list: the list
-* Return: return the len of the list
-**/
-
-size_t dlistint_len(listint_t *list)
-{
-	unsigned int i, len = 0;
-
-	for (i = 0; list != NULL; i++)
-	{
-		len++;
-		list = list->next;
-	}
-	return (len);
-}
-
-/**
  *swap_pointer - swap a node for his previous one
  *@current: node
- *@list: node list
+ *@list: Dobule linked list to sort
  *Return: return a pointer to a node which was enter it
  */
 
 listint_t *swap_pointer(listint_t *current, listint_t **list)
 {
-    listint_t *tmp = current->prev;
-    listint_t *node = current;
+	listint_t *tmp = current->prev;
+	listint_t *node = current;
 
-    tmp->next = node->next;
+	tmp->next = node->next;
 
-    if (node->next)
-        node->next->prev = tmp;
-    node->next = tmp;
+	if (node->next)
+		node->next->prev = tmp;
+	node->next = tmp;
 	node->prev = tmp->prev;
 	tmp->prev = node;
 	if (node->prev)
@@ -45,25 +26,28 @@ listint_t *swap_pointer(listint_t *current, listint_t **list)
 	return (node);
 }
 
+/**
+ * insertion_sort_list - function that sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: Dobule linked list to sort
+ * Return: void
+ **/
+
 void insertion_sort_list(listint_t **list)
 {
-    unsigned int i, len = 0;
-    listint_t *current = (*list)->next;
+	listint_t *current = (*list)->next;
 
-    if (list == NULL)
-        return;
+	if (list == NULL)
+		return;
 
-    len = dlistint_len(*list);
 
-    for (i= 0; i < len; i++)
-    {
-        while ((current->prev) && (current->prev->n > current->n))
-        {
-            current = swap_pointer(current, list);
-            print_list(*list);
-        }
-        i = 0;
-        current = current->next;
-    }
-
+	while (current)
+	{
+		while ((current->prev) && (current->prev->n > current->n))
+		{
+			current = swap_pointer(current, list);
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
